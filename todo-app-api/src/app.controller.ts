92f0abd7b09f1ca6120be9dfed6a990bfa8ad313
james <engrjayt200@gmail.com>
2021-07-23 14:09:53 +0100

    Implement MVC

--- todo-app-api/src/app.controller.ts
+++ todo-app-api/src/app.controller.ts
@@ -1,12 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Get, Controller, Render } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Render('index')
+  root() {
+    return { message: this.appService.root() };
   }
 }
